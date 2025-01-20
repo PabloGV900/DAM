ccc2f70ba45511a9de2b3a885cb687472ba991e7
PabloGV900 <pablobillar900@gmail.com>
2025-01-20 09:10:15 +0100

    suma en java

---
+++ javaprimero/suma.java
@@ -0,0 +1,18 @@
+/*Este es el algoritmo de la suma*/
+
+public class suma {   /*Esta es la clase publica a la cual e llamado igual que el archivo java  */
+
+    public static void main(String[] args) {   /*Este es el main, la parte de mi programa que se encargará de sumar los numeros */
+        
+        int num1 = 1, num2 = 2, resultado; /*declaro mis variables y su valor */
+
+        resultado = num1 + num2;   /*Aqui le estoy diciendo a mi programa que el valor de la variable resuktado
+                                     es la suma de las variables num1 y num2 */
+
+        System.out.println("la suma de " + num1 + " y de " + num2 + " es igual a: " + resultado);
+
+        /*Y aqui ya le estoy indicando que me saque por pantalla el resultado de mi operacion */
+
+    }
+
+}
