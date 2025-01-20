68ff99752067df15895069b608a929616ad02ad3
PabloGV900 <pablobillar900@gmail.com>
2025-01-20 10:12:44 +0100

    division en java

---
+++ javaprimero/division.java
@@ -0,0 +1,17 @@
+/*Este es el algoritmo de una division */
+
+public class division {
+
+    public static void main(String[] args) {
+        
+        int num1 = 6, num2 =2, resultado;   /*Aqui estoy declarando mis variables tipo int para trabajar con solo numeros enteros */
+        
+        resultado = num1 / num2;
+
+        System.out.println("El resultado de la division de " + num1 + " entre " + num2 + " es igual a: " + resultado);
+
+
+
+    }
+    
+}
