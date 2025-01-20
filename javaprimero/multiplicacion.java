c39474f28de36482f6c9dcd1f18cb2eeaa70b35f
PabloGV900 <pablobillar900@gmail.com>
2025-01-20 09:08:37 +0100

    Multiplicaciones en java

---
+++ javaprimero/multiplicacion.java
@@ -0,0 +1,17 @@
+/*Este es el algoritmo de la multiplicación */
+
+public class multiplicacion {
+    public static void main(String[] args) {
+
+        int num1 = 2, num2 = 5, resultado;   /*Aqui estoy declarando mis variables tipo int para trabajar con solo numeros enteros */
+
+        resultado = num1 * num2;    /*En esta operación le estoy indicando que multiplique mis 2 variables */
+
+        System.out.println("La multiplicacion de " + num1 + " por " + num2 + " es igual a: " + resultado);
+        
+    }
+
+
+
+    
+}
