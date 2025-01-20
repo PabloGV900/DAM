f07168b2eaafc4a595d2f68f7fd0660744fa12c0
PabloGV900 <pablobillar900@gmail.com>
2025-01-20 09:06:18 +0100

    String a Int

---
+++ javaprimero/stringaint.java
@@ -0,0 +1,14 @@
+public class stringaint{
+
+    public static void main (String[] args){
+
+        String p1 = "30", p2 = "15";
+        int n1, n2;
+
+        n1 = Integer.parseInt(p1);
+        n2 = Integer.parseInt(p2);
+
+        System.out.println(n1 + n2);
+    }
+
+}
