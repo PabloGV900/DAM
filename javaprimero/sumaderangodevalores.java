14fd863304fadd8752c75610bf71cf20ae1634ea
PabloGV900 <pablobillar900@gmail.com>
2025-01-20 09:14:55 +0100

    suma de un rango de valores

---
+++ javaprimero/sumaderangodevalores.java
@@ -0,0 +1,16 @@
+public class sumaderangodevalores {
+    
+    public static void main(String[] args) {
+        
+int num1 = 5, num2 = 99, suma = 0;
+
+        for (int i = num1; i <= num2; i++){
+            
+            suma = suma + i;
+            
+        }
+
+        System.out.println(suma);
+
+    }
+}
