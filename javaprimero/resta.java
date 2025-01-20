842c600f7908aa11e811e481e94df6717486194b
PabloGV900 <pablobillar900@gmail.com>
2025-01-20 10:11:58 +0100

    resta en java

---
+++ javaprimero/resta.java
@@ -0,0 +1,25 @@
+/*esto es el algoritmo de la resta */
+
+public class resta {
+
+    public static void main(String[] args) {
+        
+        int num1 = 10, num2 = 5, resultado;  /*Aqui declaro el valor que quiero darle a mis variables */
+
+        resultado = num1 - num2;   /*Y aqui estoy dicendo al programa que el valor de la
+                                      variable resultado sea la resta entre mis otras 2 variables */
+
+
+                                    
+        System.out.println("La resta de " + num1 + " y de " + num2 + " es igual a: " + resultado); 
+
+        /*Esto simplemente es la salida del resultado de mi operacion */
+
+
+
+
+
+
+    }
+
+}
