f995cca7ebe69d97c61614de9b218f8fc7664d22
PabloGV900 <pablobillar900@gmail.com>
2025-01-20 09:03:04 +0100

    int a String

---
+++ javaprimero/intastring.java
@@ -0,0 +1,13 @@
+public class intastring {
+
+    public static void main (String[] args){
+        String p1, p2;
+        int n1 = 20, n2 = 24;
+
+        p1 = Integer.toString((int)n1);
+        p2 = Integer.toString((int)n2);
+
+
+        System.out.println(p1 + p2);
+    }
+}
